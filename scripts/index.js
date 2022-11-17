f9828ae61ba1a819c854435af61a3dafb6487aa6
Vera Anyasova <vera.anyasova1@yandex.ru>
2022-11-17 12:18:57 +0500

    refactor: удалить лишние обертки в разметке

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,15 @@ const popupElement = document.querySelector(".popup");
 const closeButton = popupElement.querySelector(".popup__button");
 const editButton = document.querySelector(".profile__edit-button");
 const formElement = document.querySelector(".form");
-const nameInput = formElement.querySelector(".form__item_name");
-const jobInput = document.querySelector(".form__item_job");
+const nameInput = formElement.querySelector(".form__item_theme_name");
+const jobInput = formElement.querySelector(".form__item_theme_job");
 const profileName = document.querySelector(".profile__title");
 const profileJob = document.querySelector(".profile__subtitle");
 
 const openPopup = function () {
-  popupElement.classList.add("popup_opened");
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
+  popupElement.classList.add("popup_opened");
 };
 
 const closePopup = function () {
